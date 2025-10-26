e2bf4d99ab51b04fe5729b27f7279095f6e4de47
oseguri <canaein@gmail.com>
2025-10-27 05:03:48 +0900

    feat(application): main method 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,10 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = InputManager.getInput();
+
+        GameManager gm = new GameManager(input);
+
+        gm.start();
     }
 }
