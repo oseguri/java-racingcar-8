6267abeef0724e25ed635ffea6e284a7ff58b764
oseguri <canaein@gmail.com>
2025-10-27 03:19:58 +0900

    feat(inputmanager): getInput 함수 반환값 class Input 으로 변경

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -2,39 +2,35 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Pattern;
 
 public class InputManager {
 
-    public static Map<String, String> input(){
-
+    public static Input getInput() {
         String cars = Console.readLine();
         String round = Console.readLine();
 
-        if(!checkCars(cars) || !checkRounds(round)) throw new IllegalArgumentException();
+        if (!checkCars(cars) || !checkRounds(round)) throw new IllegalArgumentException();
 
-        Map<String,String> inputs = new HashMap<>();
-        inputs.put("cars", cars);
-        inputs.put("round", round);
+        List<String> carList = List.of(cars.trim().split(","));
+        int roundNum = Integer.parseInt(round);
 
-        return inputs;
+        return new Input(carList, roundNum);
     }
 
     public static Boolean checkCars(String cars) {
-        List<String> carList = List.of(cars.split(","));
-        return carList.stream().allMatch(name-> Pattern.matches(".{1,5}", name));
+        List<String> carList = List.of(cars.trim().split(","));
+
+        if(carList.stream().distinct().count() != carList.size()) return false;
+
+        return carList.stream().allMatch(
+                name -> Pattern.matches(".{1,5}", name)
+        );
     }
 
     public static Boolean checkRounds(String round) {
         return round.matches("\\d+");
     }
 
-    public static void main(String[] args) {
-        Map<String, String> input = input();
-        System.out.println(input.get("cars"));
-        System.out.println(input.get("round"));
-    }
 }
